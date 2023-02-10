a02a2932ec398d261330fc9b44fefa883a181ab8
jf <iskong9@163.com>
2023-02-10 17:50:09 +0800

    fix: 取消加密文章

--- docs/.vuepress/navbar.ts
+++ docs/.vuepress/navbar.ts
@@ -14,7 +14,7 @@ export const navbarConfig = navbar([
             {text: "文章", icon: "note", link: "/article"},
             {text: "分类", icon: "categoryselected", link: "/category"},
             {text: "标签", icon: "list", link: "/tag"},
-            {text: "加密文章", icon: "command", link: "/encrypted"},
+            // {text: "加密文章", icon: "command", link: "/encrypted"},
             {text: "收藏文章", icon: "like", link: "/star"},
             {text: "时间线", icon: "time", link: "/timeline"},
         ],
