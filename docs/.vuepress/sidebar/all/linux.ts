237629a242204cd868f34d694ada2b2c1a5fd165
topjfy <topjfk@163.com>
2023-04-25 06:10:54 +0000

    docs(linux): 添加 监控ElasticSearch进程异常脚本

--- docs/.vuepress/sidebar/all/linux.ts
+++ docs/.vuepress/sidebar/all/linux.ts
@@ -25,6 +25,7 @@ export const linuxJson =
             children: linuxBasis,
             // children: "structure",
         },
+        "monitor-elasticsearch"
         "operation-constraint",
         "command-examples",
         "exception-analysis",
