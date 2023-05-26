2b648cc6f3132594267e9650ba28015674dc0667
topjfy <topjfk@163.com>
2023-05-27 03:19:25 +0800

    Update searchPro.ts

--- docs/.vuepress/plugins/searchPro.ts
+++ docs/.vuepress/plugins/searchPro.ts
@@ -6,22 +6,12 @@ export default searchProPlugin({
     indexContent: true,
     customFields: [
         {
-            getter: (page) => page.frontmatter.title,
-            formatter: "标题：$content",
-
-        },
-        {
-            // @ts-ignore
             getter: (page) => page.frontmatter.category,
             formatter: "分类：$content",
-
         },
         {
-            // @ts-ignore
             getter: (page) => page.frontmatter.tag,
             formatter: "标签：$content",
-
         },
-
     ],
 })
