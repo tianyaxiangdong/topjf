e29e1ed24d60bf3beca92c87e8a06bbddc9dc755
topjfy <topjfk@163.com>
2023-12-07 09:38:33 +0800

    fix: update commit shell

--- commit.sh
+++ commit.sh
@@ -6,30 +6,30 @@ tag=origin
 branch=main
 
 echo ==============
-echo 提交到git仓库
+echo 提交到github仓库
 echo
 
-echo -e "\n======> 拉取新代码...\n"
+echo -e "\n=> 拉取新代码...\n"
 
 git pull
 
-echo -e "\n======> 查看本地状态...\n"
+echo -e "\n===> 查看本地状态...\n"
 
 git status
 
-echo -e "\n======> 添加更改到本地仓库缓存..."
+echo -e "\n=====> 添加所有更改到本地仓库缓存..."
 
 git add -A
 
-echo -e "\n======> 设置提交消息:\n"
+echo -e "\n=======> 设置提交消息规范:\n"
 
 pnpm run commit
 
-echo -e "\n======> 提交到远程 ${branch} 分支\n"
+echo -e "\n=========> 提交到远程 ${branch} 分支\n"
 
 git push -u $tag $branch
 
 echo
-echo ==============
-echo commit ok!
-echo ==============
+echo ==============================================
+echo =========== push to github success ===========
+echo ==============================================
